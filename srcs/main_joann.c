17a8090f7a77c831fffd7a42109b0df4a3b14d5c
Adrien <adrien.consta@gmail.com>
2021-04-08 11:30:53 +0200

    auto-commit

--- srcs/main_joann.c
+++ srcs/main_joann.c
@@ -6,7 +6,7 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 16:05:44 by jacher            #+#    #+#             */
-/*   Updated: 2021/04/07 20:00:21 by jacher           ###   ########.fr       */
+/*   Updated: 2021/04/08 10:35:40 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,26 +33,32 @@ int main(int ac, char **av)
 {
 	t_list	*token_list;
 	t_seq	*tab_seq;
-
+	char	*line;
 	tab_seq = NULL;
 
 	(void)ac;
+	(void)av;
 	token_list = NULL;
-	token_list = ft_get_token_list(token_list,av[1]);
-	if (token_list == NULL)
+	while (get_next_line(0, &line))
 	{
-		printf("pbm tokenize\n");
-		return (0);
+		token_list = ft_get_token_list(token_list, line);
+		if (token_list == NULL)
+		{
+			printf("pbm tokenize\n");
+			return (0);
+		}
+		//printf("\n*** FIRST AFTER FUNCTION ***\n");
+		//print_list(token_list);
+		assign_type_1(token_list);
+		//printf("\n*** AFTER ASSIGN 1 ***\n");
+		//print_list(token_list);
+		if (assign_type_2(token_list) == -1)
+			return(printf("wrong inputs\n"));
+		printf("\n*** AFTER ASSIGN 2 ***\n");
+		print_list(token_list);
+		free(line);
 	}
-	//printf("\n*** FIRST AFTER FUNCTION ***\n");
-	//print_list(token_list);
-	assign_type_1(token_list);
-	//printf("\n*** AFTER ASSIGN 1 ***\n");
-	//print_list(token_list);
-	if (assign_type_2(token_list) == -1)
-		return(printf("wrong inputs\n"));
-	printf("\n*** AFTER ASSIGN 2 ***\n");
-	print_list(token_list);
+	free(line);
 	create_sequence(tab_seq, token_list);
 
 	return (0);
