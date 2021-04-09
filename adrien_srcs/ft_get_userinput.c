8acb3d1eef8768612bee711d3ee1733979faf9e4
Adrien <adrien.consta@gmail.com>
2021-04-09 23:13:07 +0200

    auto-commit

--- adrien_srcs/ft_get_userinput.c
+++ adrien_srcs/ft_get_userinput.c
@@ -6,11 +6,13 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 10:30:02 by jacher            #+#    #+#             */
-/*   Updated: 2021/04/09 20:43:01 by calao            ###   ########.fr       */
+/*   Updated: 2021/04/09 23:12:32 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft.h"
+
+char	*ft_strndup(char *src, int len);
 
 int	ft_get_userinput(char **line, t_list *log)
 {
@@ -39,8 +41,9 @@ char	*ft_read_input(int fd, t_term *term, t_list *log, unsigned int log_size)
 {
 	char				buf[5];
 	int					bytes;
-	char				*screen;
+	char				**screen;
 	char				*user_input;
+	char				*list;
 	char				*to_free;
 	unsigned	int		i;
 
@@ -48,44 +51,39 @@ char	*ft_read_input(int fd, t_term *term, t_list *log, unsigned int log_size)
 	user_input = ft_strdup("");
 	if (user_input == NULL)
 		return (NULL);
-	screen = user_input;
+	screen = &user_input;
 	ft_print_prompt(term);
 	while ((bytes = read(fd, buf, 4)))
 	{
 		buf[bytes] = '\0';
 		//Analyse la chaine buf
-		if (ft_isprint(buf[0]))
+		if (ft_isprint(buf[0]) || buf[0] == 127)
 		{
-			to_free = screen;
-			screen = ft_strjoin(screen, buf);
-			free(to_free);
-			if (screen == NULL)
-				return (NULL);
-			write(1, &screen[ft_strlen(screen)], 1);
-		}
-		else
-		{
-			//fin du user_input
-			if (buf[0] == '\n')
+			if (ft_isprint(buf[0]))
 			{
-				screen[ft_strlen(screen)] = '\0';
-				write(1, &screen[ft_strlen(screen)], 1);
-				write(1, "\n", 1);
-				return (screen);
-			}
-			//backspace
-			else if (buf[0] == 127)
-			{
-				to_free = screen;
-				screen = ft_strndup(screen, ft_strlen(screen - 1));
+				//ft_join_line();	
+				to_free = *screen;
+				*screen = ft_strjoin(*screen, buf);
+				free(to_free);
 				if (screen == NULL)
 					return (NULL);
-				if (user_input == to_free)
-					user_input = screen;
-				free(to_free);
+				write(1, screen[ft_strlen(*screen)], 1);
 			}
-			else if (buf[0] == 27 && buf[1] == '[' 
-					&& (buf[2] == 'B' || buf[2] == 'A'))	
+			else
+			{
+				//ft_edit_line();
+				to_free = *screen;
+				*screen = ft_strndup(*screen, ft_strlen(*screen - 1));
+				if (screen == NULL)
+					return (NULL);
+				free(to_free);
+
+			}
+		}
+		else if (buf[0] == 27)
+		{
+			//ft_change_line_ptr();
+			if (buf[1] == '[' && (buf[2] == 'B' || buf[2] == 'A'))	
 			{
 				// A == UP
 				if (buf[2] == 'A')
@@ -93,7 +91,8 @@ char	*ft_read_input(int fd, t_term *term, t_list *log, unsigned int log_size)
 					printf("\nUP_ARROW hooked\n");
 					if (i > 0)
 						i--;
-					screen = (char *)((ft_lstat(log, i))->content);
+					list = ((char *)((ft_lstat(log, i))->content));
+					screen = &list;
 				}
 				// B == DOWN
 				else if (buf[2] == 'B')
@@ -102,20 +101,30 @@ char	*ft_read_input(int fd, t_term *term, t_list *log, unsigned int log_size)
 					if (i < log_size - 1)
 					{
 						i++;
-						screen = (char *)((ft_lstat(log, i))->content);
+						list = (char *)((ft_lstat(log, i))->content);
+						screen = &list;
 					}
 					else
 					{
 						printf("user_input = %s\n", user_input);
-						screen = user_input;
+						*screen = user_input;
 						if (i < log_size)
 							i++;
 					}
-
 				}
 			}
-			else
-				printf("\nSPECIAL_CHAR hooked.SO WHAT..?\n");
+		}
+		else if (buf[0] == '\n')
+		{
+			//ft_finish_input();
+			*screen[ft_strlen(*screen)] = '\0';
+			write(1, screen[ft_strlen(*screen)], 1);
+			write(1, "\n", 1);
+			return (*screen);
+		}
+		else
+		{
+			printf("\nSPECIAL_CHAR hooked.SO WHAT..?\n");
 		}
 		printf("i = %d\n", i);
 		//efface l'input du 
@@ -125,7 +134,7 @@ char	*ft_read_input(int fd, t_term *term, t_list *log, unsigned int log_size)
 		//imprime le prompt sur le stdout
 		ft_print_prompt(term);
 		//Imprime la ligne sur le stdout
-		write(1, screen, ft_strlen(screen));
+		write(1, *screen, ft_strlen(*screen));
 	}
 	return (NULL);
 }
