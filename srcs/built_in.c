abdeb7e8f9e846cdd891df889d29c7b14f7f306d
Adrien <adrien.consta@gmail.com>
2021-04-17 09:57:31 +0200

    auto-commit

--- srcs/built_in.c
+++ srcs/built_in.c
@@ -6,7 +6,7 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 15:28:48 by jacher            #+#    #+#             */
-/*   Updated: 2021/04/16 23:12:35 by calao            ###   ########.fr       */
+/*   Updated: 2021/04/17 09:56:25 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		find_built_in(t_simple_cmd *cmd, t_pipe *p, t_list **error, t_list **env)
 	if (ft_strcmp(cmd->job, "echo") == 0)
 	{
 		printf("ECHO FOUND!\n");
-		return(ft_echo(cmd, p));//, error, env));
+		return(ft_echo(cmd, p));
 	}
 	if (ft_strcmp(cmd->job, "env") == 0)//ADRIEN
 	{
@@ -29,17 +29,20 @@ int		find_built_in(t_simple_cmd *cmd, t_pipe *p, t_list **error, t_list **env)
 		ft_env(env);
 		return (6);
 	}
-//	if (ft_strcmp(cmd->job, "cd") == 0)//ADRIEN
+	if (ft_strcmp(cmd->job, "pwd") == 0)//JOANN
+		return (ft_pwd(cmd, p, error, env));
+
+	//	if (ft_strcmp(cmd->job, "cd") == 0)//ADRIEN
 //		return (2);
-	//if (ft_strcmp(cmd->job, "pwd") == 0)//JOANN
-	//	return (ft_pwd(cmd, p, error, env));
 	//-1 retourne si erreur malloc
 /*	if (ft_strcmp(cmd->job, "export") == 0)//ADRIEN
 		return (4);
 	if (ft_strcmp(cmd->job, "unset") == 0)//ADRIEN
 		return (5);
-		if (ft_strcmp(cmd->job, "exit") == 0)//JOANN
-		return (7);*/
+*/
+	
+	if (ft_strcmp(cmd->job, "exit") == 0)//JOANN
+		return (7);
 	return (0);
 }
 
