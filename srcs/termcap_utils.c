faa7831ed248a992b0ec681d953fde390259bf02
Adrien <adrien.consta@gmail.com>
2021-04-21 21:39:34 +0200

    auto-commit

--- srcs/termcap_utils.c
+++ srcs/termcap_utils.c
@@ -6,23 +6,21 @@
 /*   By: calao <adconsta@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 09:58:48 by calao             #+#    #+#             */
-/*   Updated: 2021/04/20 15:50:27 by calao            ###   ########.fr       */
+/*   Updated: 2021/04/21 21:39:20 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft.h"
 
-int		ft_is_endofscreen(t_term *term, char *prompt)
+int		ft_is_endofscreen(t_term *term, char *prompt, t_input *user)
 {
 	int		cur_row;
 	int		cur_col;
 
 	if (ft_getcursorxy(&cur_row, &cur_col))
-	{
-		printf("Error while reading cursors position\n");
 		return (-1);
-	}
-	if (cur_row == term->line && cur_col == term->col)
+	if ((cur_row == term->line && cur_col == term->col)
+			|| (term->col - cur_col) < (int)ft_strlen(user->screen))
 		ft_move_cursor_home(term, prompt);
 	return (0);
 }
