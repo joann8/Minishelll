8caf709abc1328d9486fdf83edada90796fd3776
Adrien <adrien.consta@gmail.com>
2021-05-03 23:23:41 +0200

    auto-commit

--- v2/srcs/search_exec_path.c
+++ v2/srcs/search_exec_path.c
@@ -6,40 +6,11 @@
 /*   By: calao <adconsta@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 21:40:11 by calao             #+#    #+#             */
-/*   Updated: 2021/05/03 12:09:28 by calao            ###   ########.fr       */
+/*   Updated: 2021/05/03 23:23:11 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft.h"
-
-int		ft_search_dir(char *exec, char *dir_path)
-{
-	struct dirent	*s_dir;
-	DIR				*d_stream;
-	char			*filepath;
-
-	d_stream = opendir(dir_path);
-	if (!d_stream)
-		return (1);
-	while ((s_dir = readdir(d_stream)) != NULL)
-	{
-		if (ft_strcmp(exec, s_dir->d_name) == 0 && s_dir->d_type == DT_REG)
-		{
-			filepath = ft_strjoin(dir_path, s_dir->d_name);
-			if (filepath == NULL)
-				return (-1);
-			if (ft_is_file_executable(filepath) == 1)
-			{
-				free(filepath);
-				closedir(d_stream);
-				return (0);
-			}
-			free(filepath);
-		}
-	}
-	closedir(d_stream);
-	return (1);
-}
 
 int		ft_init_path(char **env_path, t_list **env)
 {
